ea04b78eda23457fb4190828d537807c50b1c40d
HananAwwad <eng.hanan.awwad@gmail.com>
2016-04-23 18:38:06 +0300

    XYZ Reader
    XYZ Reader project.

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ThreeTwoFrameLayout.java
@@ -0,0 +1,24 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+
+public class ThreeTwoFrameLayout extends FrameLayout {
+    public ThreeTwoFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ThreeTwoFrameLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightSpec) {
+        int threeTwoWidth = MeasureSpec.getSize(heightSpec) * 3/2;
+        int threeTwoWidthSpec =
+                MeasureSpec.makeMeasureSpec(threeTwoWidth, MeasureSpec.EXACTLY);
+        super.onMeasure(threeTwoWidthSpec,heightSpec);
+    }
+}
